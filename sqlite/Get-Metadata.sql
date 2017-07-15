f31bbcb9015eb4d3cc333f95f97e501e18f32c8d
Dave Johnson <dave.al.johnson@gmail.com>
2017-07-15 13:49:35 -0400

    Fixed case.

--- sqlite/Get-Metadata.sql
+++ sqlite/Get-Metadata.sql
@@ -1,6 +1,6 @@
 -- Dump all table definitions
 -- Use for dumping into a file or listing at a command prompt
-select group_concat(sql, char(13, 10)) from sqlite_master where type='table';
+SELECT GROUP_CONCAT(sql, CHAR(13, 10)) FROM sqlite_master WHERE type='table';
 
 -- Use in SQL injection
-select group_concat(sql, '<br/>') from sqlite_master where type='table';
+SELECT GROUP_CONCAT(sql, '<br/>') FROM sqlite_master WHERE type='table';
