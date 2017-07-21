8621aa69c164b531d1ec4d0a8768e14be016b78c
Unknown <dave.al.johnson@gmail.com>
2017-07-21 11:24:43 -0400

    In Add-Column.sql, make new column NOT NULL by default

--- sql-server/Add-Column.sql
+++ sql-server/Add-Column.sql
@@ -2,12 +2,12 @@
 IF COL_LENGTH(N'[SchemaName].[TableName]', N'NewColumnName') IS NULL
 BEGIN
     PRINT N'Adding [SchemaName].[TableName].[NewColumnName]'
-    ALTER TABLE [SchemaName].[TableName] ADD [NewColumnName] [NewColumnType];
+    ALTER TABLE [SchemaName].[TableName] ADD [NewColumnName] [NewColumnType] NOT NULL;
 END
 
 -- With default
 IF COL_LENGTH(N'[SchemaName].[TableName]', N'NewColumnName') IS NULL
 BEGIN
     PRINT N'Adding [SchemaName].[TableName].[NewColumnName]'
-    ALTER TABLE [SchemaName].[TableName] ADD [NewColumnName] [NewColumnType] CONSTRAINT [DF_TableName_NewColumnName] DEFAULT ((0))
+    ALTER TABLE [SchemaName].[TableName] ADD [NewColumnName] [NewColumnType] NOT NULL CONSTRAINT [DF_TableName_NewColumnName] DEFAULT ((0))
 END
