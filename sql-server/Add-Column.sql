ebdaa7c815eb2e1f8f9dc76c44e19fc578f03d08
Unknown <dave.al.johnson@gmail.com>
2017-07-21 11:05:33 -0400

    Add an ADD COLUMN with named DEFAULT CONSTRAINT.

--- sql-server/Add-Column.sql
+++ sql-server/Add-Column.sql
@@ -1,5 +1,13 @@
-IF COL_LENGTH(N'[Schema].[Table]', N'NewColumn') IS NULL
+-- No default
+IF COL_LENGTH(N'[SchemaName].[TableName]', N'NewColumnName') IS NULL
 BEGIN
-    PRINT N'Adding [Schema].[Table].[NewColumn]'
-    ALTER TABLE [Schema].[Table] ADD [NewColumn] [ColumnType];
+    PRINT N'Adding [SchemaName].[TableName].[NewColumnName]'
+    ALTER TABLE [SchemaName].[TableName] ADD [NewColumnName] [NewColumnType];
+END
+
+-- With default
+IF COL_LENGTH(N'[SchemaName].[TableName]', N'NewColumnName') IS NULL
+BEGIN
+    PRINT N'Adding [SchemaName].[TableName].[NewColumnName]'
+    ALTER TABLE [SchemaName].[TableName] ADD [NewColumnName] [NewColumnType] CONSTRAINT [DF_TableName_NewColumnName] DEFAULT ((0))
 END
