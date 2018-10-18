cc917d0fec889b7fded22145a9228bb954d854d5
Dave Johnson <dave.al.johnson@gmail.com>
2018-10-18 10:05:48 -0400

    Remove check for version database property because it is not supported.

--- sql/sql-server/Backup-Databases-To-Share.sql
+++ sql/sql-server/Backup-Databases-To-Share.sql
@@ -6,8 +6,7 @@ DECLARE @timestamp datetime = CURRENT_TIMESTAMP;
 DECLARE @timestampString char(12) = FORMAT(@timestamp, 'yyyyMMddHHmm');
 DECLARE @timestampDisplayString char(16) = FORMAT(@timestamp, 'yyyy-MM-dd HH:mm');
 DECLARE @version nvarchar(64) = (SELECT CONVERT(nvarchar(64), value) FROM [DatabaseName]..fn_listextendedproperty('Version', default, default, default, default, default, default));
-DECLARE @databaseVersion nvarchar(max) = DatabaseProperty('DatabaseName','version');
-DECLARE @backupName nvarchar(max) = @BackupDirectory + '\' + @version + '_' + @timestampString + '_' + @databaseVersion + '_DatabaseName.bak';
+DECLARE @backupName nvarchar(max) = @BackupDirectory + '\' + @version + '_' + @timestampString + '_DatabaseName.bak';
 
 RAISERROR('Backing up Database Version %s at %s', 0, 1, @version, @timestampDisplayString) WITH NOWAIT;
 
