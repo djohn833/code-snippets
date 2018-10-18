8e3ec520132969fcb2a58bf6fd58fe5cfc128cec
Dave Johnson <dave.al.johnson@gmail.com>
2018-10-18 09:43:45 -0400

    Organize backup script variables

--- sql/sql-server/Backup-Databases-To-Share.sql
+++ sql/sql-server/Backup-Databases-To-Share.sql
@@ -1,17 +1,13 @@
+USE [master]
+
 DECLARE @BackupDirectory nvarchar(max) = 'C:\Share';
+
 DECLARE @timestamp datetime = CURRENT_TIMESTAMP;
 DECLARE @timestampString char(12) = FORMAT(@timestamp, 'yyyyMMddHHmm');
 DECLARE @timestampDisplayString char(16) = FORMAT(@timestamp, 'yyyy-MM-dd HH:mm');
-DECLARE @version nvarchar(64);
-DECLARE @backupName nvarchar(max);
-
-USE [DatabaseName]
-SELECT @version = CONVERT(nvarchar(64), value) FROM fn_listextendedproperty('Version', default, default, default, default, default, default);
-
-SET @backupName = @BackupDirectory + '\' + @version + '_' + @timestampString + '_DatabaseName.bak';
-
-
-USE [master]
+DECLARE @version nvarchar(64) = (SELECT CONVERT(nvarchar(64), value) FROM [DatabaseName]..fn_listextendedproperty('Version', default, default, default, default, default, default));
+DECLARE @databaseVersion nvarchar(max) = DatabaseProperty('DatabaseName','version');
+DECLARE @backupName nvarchar(max) = @BackupDirectory + '\' + @version + '_' + @timestampString + '_' + @databaseVersion + '_DatabaseName.bak';
 
 RAISERROR('Backing up Database Version %s at %s', 0, 1, @version, @timestampDisplayString) WITH NOWAIT;
 
