50c17594769f20955d33124e2c514a803d7eb584
Dave Johnson <dave.al.johnson@gmail.com>
2019-03-19 14:54:22 -0400

    Add @SearchString variable and add a SELECT for the matching rows.

--- database/sql-server/For-All-Columns.sql
+++ database/sql-server/For-All-Columns.sql
@@ -1,9 +1,17 @@
 SET NOCOUNT ON;
 
+-- Configurable variables. Set these as needed.
+DECLARE @SearchString NVARCHAR(MAX) = 'SearchString';
+-- End configurable variables.
+
 DECLARE column_cursor CURSOR FOR
-SELECT N'[' + SCHEMA_NAME(tables.schema_id) + N'].[' + OBJECT_NAME(all_columns.object_id) + N']', all_columns.name
+SELECT
+	N'[' + SCHEMA_NAME(tables.schema_id) + N'].[' + OBJECT_NAME(all_columns.object_id) + N']',
+	N'[' + all_columns.name + N']',
+	N'[' + SCHEMA_NAME(tables.schema_id) + N'.' + OBJECT_NAME(all_columns.object_id) + N'.' + all_columns.name + N']'
 FROM sys.all_columns
 JOIN sys.tables ON tables.object_id = all_columns.object_id
+JOIN sys.types ON types.user_type_id = all_columns.user_type_id
 ORDER BY SCHEMA_NAME(tables.schema_id), OBJECT_NAME(all_columns.object_id), all_columns.name;
 OPEN column_cursor;
 
@@ -14,9 +22,9 @@ DECLARE @MatchingColumns TABLE (
 
 WHILE 1 = 1
 BEGIN
-	DECLARE @table_full_name NVARCHAR(MAX), @column_name SYSNAME, @match BIT;
+	DECLARE @table_full_name NVARCHAR(MAX), @column_name SYSNAME, @column_alias NVARCHAR(MAX), @match BIT = 0;
 
-	FETCH NEXT FROM column_cursor INTO @table_full_name, @column_name;
+	FETCH NEXT FROM column_cursor INTO @table_full_name, @column_name, @column_alias;
 
 	IF @@FETCH_STATUS <> 0 BREAK
 
@@ -25,17 +33,25 @@ BEGIN
 	-- Use @table_full_name and @column_name here
 
 	-- For example, find columns with values that match a certain criteria.
-	DECLARE @sql NVARCHAR(MAX) = N'IF EXISTS(SELECT TOP 1 0 FROM ' + @table_full_name + ' WHERE CONVERT(VARCHAR(MAX), ' + @column_name + ') LIKE ''%StringToFind%'') SELECT @match = 1;';
+	SET NOEXEC OFF -- Can switch between OFF and ON to turn the following on and off, respectively.
+
+	DECLARE @sql NVARCHAR(MAX) = N'IF EXISTS(SELECT TOP 1 0 FROM ' + @table_full_name + ' WHERE CONVERT(VARCHAR(MAX), ' + @column_name + ') LIKE ''%'' + @SearchString + ''%'') SELECT @match = 1;';
 	EXEC sp_executesql @sql,
-		N'@table_full_name NVARCHAR(MAX), @column_name SYSNAME, @match BIT OUTPUT',
-		@table_full_name = @table_full_name,
-		@column_name = @column_name,
+		N'@SearchString NVARCHAR(MAX), @match BIT OUTPUT',
+		@SearchString = @SearchString,
 		@match = @match OUTPUT;
 
 	IF @match = 1
 	BEGIN
 		INSERT INTO @MatchingColumns (TableFullName, ColumnName) VALUES (@table_full_name, @column_name);
+
+		SELECT @sql = N'SELECT CONVERT(VARCHAR(MAX), ' + @column_name + ') AS ' + @column_alias + ' FROM ' + @table_full_name + ' WHERE CONVERT(VARCHAR(MAX), ' + @column_name + ') LIKE ''%'' + @SearchString + ''%'';';
+		EXEC sp_executesql @sql,
+			N'@SearchString NVARCHAR(MAX)',
+			@SearchString = @SearchString;
 	END
+
+	SET NOEXEC OFF
 END
 
 CLOSE column_cursor;
