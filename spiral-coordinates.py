5f018f6e4223c1f87a8d2aca463209486edc5bb0
Dave Johnson <dave.al.johnson@gmail.com>
2019-10-17 19:47:13 -0400

    Simplify spiral-coordinates.py

--- spiral-coordinates.py
+++ spiral-coordinates.py
@@ -1,3 +1,6 @@
+def add(x, y):
+    return (x[0] + y[0], x[1] + y[1])
+
 deltas = [(0, -1), (1, 0), (0, 1), (-1, 0)]
 vals = {}
 vals[(0, 0)] = 1
@@ -7,10 +10,10 @@ d = 0
 
 for i in range(2, 4000):
     nextd = (d + 1) % 4
-    nextp = (p[0] + deltas[nextd][0], p[1] + deltas[nextd][1])
+    nextp = add(p, deltas[nextd])
     if nextp in vals:
         nextd = d
-        nextp = (p[0] + deltas[nextd][0], p[1] + deltas[nextd][1])
+        nextp = add(p, deltas[nextd])
     vals[nextp] = i
     d, p = nextd, nextp
 
@@ -19,19 +22,17 @@ knight = [(2, 1), (-2, 1), (2, -1), (-2, -1), (1, 2), (-1, 2), (1, -2), (-1, -2)
 p = (0, 0); visited = set([(0, 0)]); visitedNums = [1]
 
 while True:
-    for d in knight:
-        nextp = (p[0] + d[0], p[1] + d[1])
-        if nextp not in visited:
-            break
-    else:
-        print('Stuck at %d' % vals[p])
-        break
     minNum = 999999999
+    canMove = False
     for d in knight:
-        nextp = (p[0] + d[0], p[1] + d[1])
+        nextp = add(p, d)
         if vals[nextp] < minNum and nextp not in visited:
+            canMove = True
             minNum = vals[nextp]
             minNextP = nextp
+    if not canMove:
+        print('Stuck at %d' % vals[p])
+        break
     p = minNextP
     visited.add(p)
     visitedNums.append(vals[p])
