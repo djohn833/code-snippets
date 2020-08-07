57b5eb83544126b4c1f11faac4760c4f14f7169c
Dave Johnson <dave.al.johnson@gmail.com>
2020-08-07 15:22:22 -0400

    Scraping alternativejs.com and saving results.

--- web-scraping/index.ts
+++ web-scraping/index.ts
@@ -1,1 +1,28 @@
-console.log('Hello TypeScript!')
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+
+async function main() {
+  const browser = await puppeteer.launch({
+    headless: true
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto('http://www.alternativejs.com/');
+
+  const closeModal = (await page.$x('//*[@id="PopupSignupForm_0"]/div[2]/div[1]'))[0];
+  await closeModal.click();
+
+  let myElement = (await page.$x('//*[@id="mainNav"]/div/a'))[0];
+  let titleProp = await myElement.getProperty('innerText');
+  let title = await titleProp.jsonValue();
+  console.log({title})
+
+  await page.screenshot({ path: './myPage.png' });
+
+  fs.writeFileSync('./myData.txt', title);
+
+  await browser.close();
+}
+
+main();
